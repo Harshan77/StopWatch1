2cdd8af951fcf8bea0dd0507b7c82dc2cf512ad4
harshan77-admin <harshanveerabathini777@gmail.com>
2025-02-06 10:22:20 +0530

    Added all html elements and styled the elements

--- stopWatch1/src/StopWatch.jsx
+++ stopWatch1/src/StopWatch.jsx
@@ -7,10 +7,35 @@ function stopWatch(){
     const intervalRef=useRef(null);
     const startTimeRef=useRef(0);
 
+    useEffect(() => {
+
+    },[isRunning]);
+
+    function start(){
+
+    }
+
+    function stop(){
+
+    }
+
+    function reset(){
+
+    }
+
+    function formatTime(){
+        return "00:00:00";
+    }
+
     return(
-        <>
-        
-        </>
+        <div className="stopwatch">
+            <div className="display">{formatTime()}</div>
+            <div className="controls">
+                <div onClick={start} className="start-button">Start</div>
+                <div onClick={stop} className="stop-button">Stop</div>
+                <div onClick={reset} className="reset-button">Reset</div>
+            </div>
+        </div>
     );
 }
 
