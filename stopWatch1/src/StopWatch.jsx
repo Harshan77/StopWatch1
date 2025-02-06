a4c369eb74ec6b284cc02d999ff6a58b89ef8926
harshan77-admin <harshanveerabathini777@gmail.com>
2025-02-06 10:04:15 +0530

    added State values

---
+++ stopWatch1/src/StopWatch.jsx
@@ -0,0 +1,17 @@
+import React,{useState,useEffect,useRef} from "react";
+
+function stopWatch(){
+
+    const [isRunning,setIsRunning] = useState(false);
+    const [elapsedTime,setElapsedTime]=useState(0);
+    const intervalRef=useRef(null);
+    const startTimeRef=useRef(0);
+
+    return(
+        <>
+        
+        </>
+    );
+}
+
+export default stopWatch;
